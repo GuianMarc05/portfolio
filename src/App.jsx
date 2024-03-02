6783c8f889645b06cef3091058722c7ef45f0f4a
GuianMarc05 <Marccosmo@yahoo.com>
2024-03-03 00:03:31 +0800

    love

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css';
 
 function App() {
-	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</>
-	);
+  return (
+    <>
+      <div className="flex items-center justify-between m-10">
+        <div>
+          <h1>Cosmo</h1>
+        </div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Contact Me</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
