23caf3e0dd509b39e8dd557197ab0b050e44ccf9
GuianMarc05 <141669247+GuianMarc05@users.noreply.github.com>
2024-03-04 16:59:23 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <div className="flex items-center justify-between m-10">
-        <div>
-          <h1>Cosmo</h1>
-        </div>
-        <div>
-          <ul>
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Projects</li>
-            <li>Contact Me</li>
-          </ul>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="flex items-center justify-between font-kodchasan">
+				<div className="flex m-10">
+					<h1 className="text-3xl text-white font-bold cursor-pointer">
+						Cosmo
+					</h1>
+				</div>
+				<div className="list-none flex">
+					<ul className="flex text-2xl text-white cursor-pointer">
+						<li className="mr-7">Home</li>
+						<li className="mr-7">About Me</li>
+						<li className="mr-7">Projects</li>
+						<li className="mr-7">Contact Me</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
