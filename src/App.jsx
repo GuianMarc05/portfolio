f21785c1760a4f9fa86c142fae293536ef3c162b
GuianMarc05 <141669247+GuianMarc05@users.noreply.github.com>
2024-07-15 13:38:51 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,156 @@
+import { useRef } from 'react';
 import './App.css';
+import image from './assets/pic.png';
+import pup from './assets/pup.jpg';
+import bg from './assets/background.svg';
 
 function App() {
+	// Ref for the section you want to scroll to
+	const pupRef = useRef(null);
+
+	// Function to handle scrolling to the section
+	const scrollToPup = () => {
+		pupRef.current.scrollIntoView({ behavior: 'smooth' });
+	};
+
 	return (
 		<>
-			<div className="flex items-center justify-between font-kodchasan">
-				<div className="flex m-10">
-					<h1 className="text-3xl text-white font-bold cursor-pointer">
-						Cosmo
-					</h1>
-				</div>
-				<div className="list-none flex">
-					<ul className="flex text-2xl text-white cursor-pointer">
-						<li className="mr-7">Home</li>
-						<li className="mr-7">About Me</li>
-						<li className="mr-7">Projects</li>
-						<li className="mr-7">Contact Me</li>
+			<div
+				className="grid gap-8 bg-repeat-y bg-cover bg-local md:gap-24 lg:gap-28 xl:gap-32 2xl:gap-36"
+				style={{ backgroundImage: `url(${bg})` }}
+			>
+				<div className="m-2 p-2 flex items-center justify-between text-white font-poppins">
+					<div className="text-sm font-semibold md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+						<h1>Cosmo</h1>
+					</div>
+					<ul className="flex flex-row items-end cursor-pointer text-sm md:text-lg lg:text-xl xl:text-2xl">
+						<li className="mr-2 md:mr-3 lg:mr-4 xl:mr-6" onClick={scrollToPup}>
+							About Me
+						</li>
+						<li className="mr-2 md:mr-3 lg:mr-4 xl:mr-6">Projects</li>
+						<li>Contact</li>
 					</ul>
+				</div>
+				<div className="m-2 p-2 flex flex-col items-center justify-center text-center text-white md:flex-row xl:items-center justify-around">
+					<div className="font-poppins">
+						<div className="text-lg font-bold md:text-2xl lg:text-4xl xl:text-6xl 2xl:text-8xl">
+							<h1>Hi! I am Guian.</h1>
+						</div>
+						<div className="text-sm md:text-lg lg:text-xl xl:text-2xl">
+							<p>
+								A front-end developer, A self-motivated, goal-oriented and
+								aspiring web developer.
+							</p>
+						</div>
+					</div>
+					<div className="transition ease-in-out delay-150 hover:scale-105">
+						<img src={image} alt="Guian Cosmo" />
+					</div>
+				</div>
+				<div
+					className="m-2 p-2 flex flex-col items-center justify-center md:flex-row xl:items-center justify-around font-poppins"
+					ref={pupRef}
+				>
+					<div>
+						<img
+							className="transition ease-in-out delay-150 h-96 w-auto rounded-2xl hover:scale-105"
+							src={pup}
+							alt="PUP"
+						/>
+					</div>
+					<div className="flex flex-col p-2 items-center justify-center text-center text-white">
+						<div className="text-lg font-medium md:text-xl lg:text-2xl xl:text-4xl 2xl:text-6xl">
+							<h1>Graduated in PUP - San Juan Campus</h1>
+						</div>
+						<div className="text-sm md:text-lg lg:text-xl xl:text-2xl mt-5">
+							<p>
+								Achieved Latin Honor - Cum Laude in Bachelor of Science in
+								Information Technology
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="m-2 p-2 flex flex-col items-center justify-center text-center text-white font-poppins xl:items-center justify-around">
+					<div className="text-lg font-medium md:text-xl lg:text-2xl xl:text-4xl 2xl:text-6xl">
+						<h1>Mobile and Web Front-End Developer</h1>
+					</div>
+					<div className="text-lg md:text-xl lg:text2xl xl:text-3xl mt-5">
+						<p>
+							at{' '}
+							<a href="https://odrtechinc.com/" className="cursor:pointer">
+								ODR Technologies
+							</a>
+						</p>
+					</div>
+				</div>
+				<div className="m-2 p-2 flex flex-col items-center justify-center text-center text-white font-poppins xl:items-center justify-around">
+					<div className="text-lg font-medium md:text-xl lg:text-2xl xl:text-4xl 2xl:text-6xl">
+						<h1>Tools</h1>
+					</div>
+					<div className="flex flex-nowrap overflow-hidden mt-5">
+						<div className="flex flex-column gap-6">
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/616ac2b89b9e754fd86e01be484727f16dd4a4371c05fa773e98f4d36d237842?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="size-14 md:size-16 lg:size-20 xl:size-24 2xl:size-32"
+								alt="HTML"
+							/>
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/05127424943f296a72dc58ef1e92d94ebe83841d9df675d827b4c5f798b070b7?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="size-14 md:size-16 lg:size-20 xl:size-24 2xl:size-32"
+								alt="CSS"
+							/>
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/172bd185ed33f231af82bd515c9dd87d509c51abc3bc624eef6eeafaf67e3f08?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="size-14 md:size-16 lg:size-20 xl:size-24 2xl:size-32"
+								alt="JS"
+							/>
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/1eb04e2f859310d6d4eccd376a460b901b56eb6b2fa92217dc4a8f4a11f4d422?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="size-14 md:size-16 lg:size-20 xl:size-24 2xl:size-32"
+								alt="TS"
+							/>
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/a323a3aec822d878ccf7a9de8975ddb4bde199b0b87da2b46923fab957989b1c?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="size-14 md:size-16 lg:size-20 xl:size-24 2xl:size-32"
+								alt="React"
+							/>
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/ccc5d287d6efb7758a758917374fa0e8b86849e09ba6e55d095372c016cdd950?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="size-14 md:size-16 lg:size-20 xl:size-24 2xl:size-32"
+								alt="Angular"
+							/>
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/d37acf2ef34693d301ed4ec8276dc9b355b8ad76ac367309bb59894126d16bb0?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="w-24 h-14 md:w-28 h-16 lg:w-32 h-16 xl:w-36 h-20 2xl:w-48 h-32"
+								alt="Tailwind"
+							/>
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/b77a361cdb4ce67a4d431b8b72ded10ac468090dd84b8ca95c95b467a29528a5?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="w-24 h-14 md:w-28 h-16 lg:w-32 h-16 xl:w-36 h-14 2xl:w-48 h-32"
+								alt="Ionic"
+							/>
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/bfbf8041f3e7251fc02352ddede1a7d8a0e069bac067b6f3c845d3ccb934a9ea?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="size-14 md:size-16 lg:size-20 xl:size-24 2xl:size-32"
+								alt="C"
+							/>
+							<img
+								loading="lazy"
+								src="https://cdn.builder.io/api/v1/image/assets/TEMP/c443de47b4b67d3e6703bece50a7f16ef34b6e2ecb69514cc63b7cb9f1db6780?apiKey=0a8c0fd347f942b0be298d8b3405a882&"
+								className="size-14 md:size-16 lg:size-20 xl:size-24 2xl:size-32"
+								alt="Mysql"
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
+import { transform } from 'framer-motion';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				kodchasan: ['Kodchasan'],
+				poppins: ['Poppins'],
+			},
+			animation: {
+				'loop-scroll': 'loop-scroll 50s linear infinite',
+			},
+			keyframes: {
+				'loop-scroll': {
+					from: { transform: 'translateX(0)' },
+					to: { transform: 'translateX(-100%)' },
+				},
 			},
 		},
 	},
